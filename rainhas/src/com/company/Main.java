da41647e9f932bdb2d48905b87b34cbe73ba698b
spideryzarc <spideryzarc@gmail.com>
2020-02-21 09:51:59 -0300

    método enumerativo

--- rainhas/src/com/company/Main.java
+++ rainhas/src/com/company/Main.java
@@ -3,23 +3,12 @@ package com.company;
 import java.util.Arrays;
 import java.util.Random;
 
+import static java.util.Arrays.fill;
+
 public class Main {
 
     public static int ataque(int sol[]){
-        int a = 0;
-        //supondo que sol é um arranjo de 0 ... n
-        //diagonal principal
-        for (int i = 0; i < sol.length; i++) {
-            for (int j = i+1; j < sol.length; j++) {
-                if(sol[i]-i == sol[j]-j){
-                    a++;
-                }
-                if(sol[i]+i == sol[j]+j){
-                    a++;
-                }
-            }
-        }
-        return a;
+        return ataque(sol,sol.length-1);
     }
 
 
@@ -39,8 +28,9 @@ public class Main {
 
 
     public static void shuffle(int sol[]){
-        for (int i = sol.length-1; i > 1 ; i--) {
-            int x = rd.nextInt(i);
+        int n = sol.length;
+        for (int i = 0; i < n; i++) {
+            int x = rd.nextInt(n);
             int aux = sol[i];
             sol[i] = sol[x];
             sol[x] = aux;
@@ -49,29 +39,76 @@ public class Main {
 
     public static Random rd = new Random(7);
 
-    public static void main(String[] args) {
-	    int n = 20; //representação do problema
-        int sol[] = new int[n];
-        for (int i = 0; i < n; i++)
-            sol[i]=i;
+    public static boolean posicione(int sol[], int i, boolean linhaOcupada[]){
+          for (int j = 0; j < sol.length; j++)
+            if(!linhaOcupada[j]){
+                sol[i] = j;
+                if(ataque(sol,i) == 0){
+                    if(i == sol.length-1){
+                        return true;//ancora da recursão
+                    }
 
-        int bestcost = Integer.MAX_VALUE;
-        int bestSol[] = new int[n];
-        for (int i = 0; i < 100000; i++) {
-            shuffle(sol);
-//            System.out.println(Arrays.toString(sol));
-            int cost = ataque(sol);
-            if (cost < bestcost) {
-                System.out.println(cost);
-                bestcost = cost;
-                for (int j = 0; j < sol.length; j++)
-                    bestSol[j] = sol[j];
-                if(cost == 0)
-                    break;
+                    linhaOcupada[j] = true;
+                    if(posicione(sol,i+1,linhaOcupada))
+                        return true;
+                    linhaOcupada[j] = false;
+                }
+            }
+          return false;
+    }
+
+    public static int[] enumere(int n){
+        int[] sol = new int[n];
+        posicione(sol,0,new boolean[n]);
+        return sol;
+    }
+
+    private static int ataque(int[] sol, int n) {
+        int a = 0;
+        //supondo que sol é um arranjo de 0 ... n-1
+        //diagonal principal
+        for (int i = 0; i <= n; i++) {
+            for (int j = i+1; j <= n; j++) {
+                if(sol[i]-i == sol[j]-j){
+                    a++;
+                }
+                if(sol[i]+i == sol[j]+j){
+                    a++;
+                }
             }
         }
-        System.out.println(Arrays.toString(bestSol));
-        print(bestSol);
+        return a;
+    }
+
+    public static void main(String[] args) {
+	    int n = 25; //representação do problema
+        int sol[] = enumere(n);
+        System.out.println(ataque(sol));
+        print(sol);
+
+
+//        int current[] = new int[n];
+//        //incializa como um arranjo 0..n-1
+//        for (int i = 0; i < n; i++)
+//            current[i]=i;
+//
+//        int bestCost = Integer.MAX_VALUE;
+//        int bestSol[] = new int[n];
+//        for (int i = 0; i < 10000000; i++) {
+//            shuffle(current);
+////            System.out.println(Arrays.toString(current));
+//            int cost = ataque(current);
+//            if (cost < bestCost) {
+//                System.out.println(cost);
+//                bestCost = cost;
+//                for (int j = 0; j < current.length; j++)
+//                    bestSol[j] = current[j];
+//                if(cost == 0)
+//                    break;
+//            }
+//        }
+//        System.out.println(Arrays.toString(bestSol));
+//        print(bestSol);
 
 
 
