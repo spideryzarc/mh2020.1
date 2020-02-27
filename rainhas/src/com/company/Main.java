af02e9c10b9588da09053062435592005e75f9c3
spideryzarc <spideryzarc@gmail.com>
2020-02-27 09:45:18 -0300

    método enumerativo, função de avaliação otimizada

--- rainhas/src/com/company/Main.java
+++ rainhas/src/com/company/Main.java
@@ -7,19 +7,19 @@ import static java.util.Arrays.fill;
 
 public class Main {
 
-    public static int ataque(int sol[]){
-        return ataque(sol,sol.length-1);
+    public static int ataque(int sol[]) {
+        return ataque(sol, sol.length - 1);
     }
 
 
-    public static void print(int sol[]){
+    public static void print(int sol[]) {
         for (int i = 0; i < sol.length; i++) {
             System.out.print("|");
             for (int j = 0; j < sol[i]; j++) {
                 System.out.print(" |");
             }
             System.out.print("x|");
-            for (int j = sol[i]+1; j < sol.length; j++) {
+            for (int j = sol[i] + 1; j < sol.length; j++) {
                 System.out.print(" |");
             }
             System.out.println();
@@ -27,7 +27,7 @@ public class Main {
     }
 
 
-    public static void shuffle(int sol[]){
+    public static void shuffle(int sol[]) {
         int n = sol.length;
         for (int i = 0; i < n; i++) {
             int x = rd.nextInt(n);
@@ -39,27 +39,27 @@ public class Main {
 
     public static Random rd = new Random(7);
 
-    public static boolean posicione(int sol[], int i, boolean linhaOcupada[]){
-          for (int j = 0; j < sol.length; j++)
-            if(!linhaOcupada[j]){
+    public static boolean posicione(int sol[], int i, boolean linhaOcupada[]) {
+        for (int j = 0; j < sol.length; j++)
+            if (!linhaOcupada[j]) {
                 sol[i] = j;
-                if(ataque(sol,i) == 0){
-                    if(i == sol.length-1){
+                if (ataqueUltimo(sol, i) == 0) {
+                    if (i == sol.length - 1) {
                         return true;//ancora da recursão
                     }
 
                     linhaOcupada[j] = true;
-                    if(posicione(sol,i+1,linhaOcupada))
+                    if (posicione(sol, i + 1, linhaOcupada))
                         return true;
                     linhaOcupada[j] = false;
                 }
             }
-          return false;
+        return false;
     }
 
-    public static int[] enumere(int n){
+    public static int[] enumere(int n) {
         int[] sol = new int[n];
-        posicione(sol,0,new boolean[n]);
+        posicione(sol, 0, new boolean[n]);
         return sol;
     }
 
@@ -68,11 +68,11 @@ public class Main {
         //supondo que sol é um arranjo de 0 ... n-1
         //diagonal principal
         for (int i = 0; i <= n; i++) {
-            for (int j = i+1; j <= n; j++) {
-                if(sol[i]-i == sol[j]-j){
+            for (int j = i + 1; j <= n; j++) {
+                if (sol[i] - i == sol[j] - j) {
                     a++;
                 }
-                if(sol[i]+i == sol[j]+j){
+                if (sol[i] + i == sol[j] + j) {
                     a++;
                 }
             }
@@ -80,11 +80,33 @@ public class Main {
         return a;
     }
 
+    private static int ataqueUltimo(int[] sol, int n) {
+        int a = 0;
+        //supondo que sol é um arranjo de 0 ... n-1
+        //diagonal principal
+        for (int i = 0; i < n; i++) {
+            if (sol[i] - i == sol[n] - n) {
+                a++;
+            }
+            if (sol[i] + i == sol[n] + n) {
+                a++;
+            }
+
+        }
+        return a;
+    }
+
     public static void main(String[] args) {
-	    int n = 25; //representação do problema
+        int n = 32; //representação do problema
+
+        long t0 = System.currentTimeMillis();
         int sol[] = enumere(n);
+        long t = System.currentTimeMillis()-t0;
+
         System.out.println(ataque(sol));
         print(sol);
+        System.out.println(t);
+
 
 
 //        int current[] = new int[n];
@@ -109,8 +131,6 @@ public class Main {
 //        }
 //        System.out.println(Arrays.toString(bestSol));
 //        print(bestSol);
-
-
 
 
     }
